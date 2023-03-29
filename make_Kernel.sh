6169e5cdeb7bc7125043db83a70abf3c7afc43d1
samshen1017 <samshen1017@sina.com>
2023-03-29 20:16:59 +0800

    编译通过，默认config，详细参考README.HOW_TO.txt

---
+++ make_Kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+KERNEL_DIR=$PWD/linux-5.15.67
+ENV_SET=/home/isaac/toolchain/st/stm32mp1/4.0.4-openstlinux-5.15-yocto-kirkstone-mp1-v22.11.23/environment-setup-cortexa7t2hf-neon-vfpv4-ostl-linux-gnueabi
+
+cd $KERNEL_DIR
+
+#设置编译器
+source $ENV_SET
+
+if [ $1 == "debug" ]; then
+    unset -v CFLAGS LDFLAGS
+    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" stm32mp157c_astro_defconfig
+    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" uImage dtbs modules LOADADDR=0XC2000040 -j12
+elif [ $1 == "clean" ]; then
+    make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" distclean
+    rm -rf $PWD/../build
+    rm -rf $PWD/../deploy
+fi
+

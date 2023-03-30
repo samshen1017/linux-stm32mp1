051a05c77fa787f6975672f918400ae9b73bddd3
墨鸿苍穹 <samshen1017@sina.com>
2023-03-30 19:11:42 +0800

    up

--- make_Kernel.sh
+++ make_Kernel.sh
@@ -8,10 +8,15 @@ cd $KERNEL_DIR
 #设置编译器
 source $ENV_SET
 
-if [ $1 == "debug" ]; then
+if [ $1 == "astro" ]; then
     unset -v CFLAGS LDFLAGS
     make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" stm32mp157c_astro_defconfig
     make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" uImage dtbs modules LOADADDR=0XC2000040 -j12
+
+elif [ $1 == "copy" ]; then
+    cp $PWD/../build/arch/arm/boot/uImage ../../image
+    cp $PWD/../build/arch/arm/boot/dts/stm32mp157c-astro.dtb ../../image
+
 elif [ $1 == "clean" ]; then
     make ARCH=arm CROSS_COMPILE=arm-ostl-linux-gnueabi- O="$PWD/../build" distclean
     rm -rf $PWD/../build
